52a845a5833f29e73a797718a3aeb8818dcd6311
vankodg <vankodaniel97@gmail.com>
2022-09-22 00:50:43 +0200

    fetch data from api

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
+type Category =
+  | "housing"
+  | "travel"
+  | "food"
+  | "utilities"
+  | "insurance"
+  | "healthcare"
+  | "financial"
+  | "lifestyle"
+  | "entertainment"
+  | "miscellaneous";
+
+type Currency = "HUF" | string;
+
+type Transaction = Readonly<{
+  id: number;
+  summary: string;
+  category: Category;
+  sum: number;
+  currency: Currency;
+  paid: Date;
+}>;
+
+export default function App() {
+  const [result, setResult] = useState<Transaction[]>([]);
+
+  useEffect(() => {
+    const api = async () => {
+      const data = await fetch(
+        "https://development.sprintform.com/transactions.json",
+        {
+          method: "GET",
+        }
+      );
+      const jsonData = await data.json();
+      setResult(jsonData);
+    };
+
+    api();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -19,8 +59,11 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+        {result.map((transaction) => (
+          <div>{transaction.summary}</div>
+        ))}
+      </div>
     </div>
   );
 }
-
-export default App;
