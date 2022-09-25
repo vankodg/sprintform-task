a60a192b212337846e6b17d77d1b8cbf44e279c5
vankodg <vankodaniel97@gmail.com>
2022-09-25 19:59:42 +0200

    include bootstrap stylesheet

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,12 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
+      integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor"
+      crossOrigin="anonymous"
+    />
     <App />
   </React.StrictMode>
 );
